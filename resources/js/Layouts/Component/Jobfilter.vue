94b9eaf082c7e765d1b315972f414956ba6d0e94
Syllar <ctvanlalropuia@gmail.com>
2023-06-07 23:37:44 +0530

    Change for mobile view

--- resources/js/Layouts/Component/Jobfilter.vue
+++ resources/js/Layouts/Component/Jobfilter.vue
@@ -1,19 +1,21 @@
 <template>
     <form @submit.prevent="filter">
-        <div class="m-3 grid grid-rows-1">
-            <div class="mt-2 flex flex-wrap gap-2 justify-start mr-3">
-                <div class="xl:flex items-center gap-1 lg:grid grid-rows-2 md:gap-2">
-                    <!-- <select name="Category" id="Category"
+        <!-- <div class="m-3 flex bg-green-300"> -->
+        <div class="m-3 mt-2 md:flex gap-2 justify-start mr-3">
+            <!-- <div class="xl:flex items-center gap-1 lg:grid grid-rows-2 md:gap-2"> -->
+            <div class="xl:flex items-center gap-1 lg:grid grid-rows-1 md:gap-2 w-full xl:w-auto md:w-auto">
+                <!-- <select name="Category" id="Category"
                         class="block rounded-tl-lg rounded-bl-lg shadow-sm border-gray-300 text-gray-500 text-sm">
                         <option value="null">-- Qualification --</option>
                        </select>
 
                     <input type="text" placeholder="Applicant Name" class="border border-gray-300 text-sm"> -->
-                    <input v-model="filterForm.job_title" type="text" placeholder="Job title"
-                        class="border border-gray-300 rounded-lg w-full mb-2 lg:border-gray-300 text-sm md:border-gray-300 sm:border-gray-300 sm:mb-2" />
-                    <input v-model="filterForm.department" type="text" placeholder="Department"
-                        class="border border-gray-300 rounded-lg w-full mb-2 lg:border-gray-300 text-sm md:border-gray-300 sm:border-gray-300 sm:mb-2" />
-                </div>
+                <input v-model="filterForm.job_title" type="text" placeholder="Job title"
+                    class="border border-gray-300 rounded-lg mb-2 lg:border-gray-300 text-sm md:border-gray-300 sm:border-gray-300 sm:mb-2" />
+                <input v-model="filterForm.department" type="text" placeholder="Department"
+                    class="border border-gray-300 rounded-lg mb-2 lg:border-gray-300 text-sm md:border-gray-300 sm:border-gray-300 sm:mb-2" />
+            </div>
+            <div class="md:flex md:gap-2 flex gap-2">
                 <button type="submit"
                     class="w-24 bg-gray-200 p-2 mb-2 rounded-md hover:bg-gray-400 text-sm flex flex-row justify-evenly items-center text-center">
                     Search
@@ -29,6 +31,7 @@
                 </button>
             </div>
         </div>
+        <!-- </div> -->
     </form>
 </template>
 
